61741619b2ec2be3371567b5f94f26c471622710
MohamedMBG <baghdadmohamed.me@gmail.com>
2023-09-22 10:40:45 -0700

    task 1 modified

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+#this code will install the pckage puppet-lint
+package{ 'falsk':
+	ensure => '2.1.0',
+	provider => 'pip3',
 }
